355dbe72bbf118694dfb7395969e9bfe88517d84
Fernando Tesone <fenatesone@gmail.com>
2018-12-05 01:35:14 -0300

    Corrección en ejercicio 5 - Contar vehículos distintos

--- Ejercicio5.py
+++ Ejercicio5.py
@@ -12,13 +12,13 @@ data_rdd = sc.textFile(sys.argv[1])
 franja = int(sys.argv[3])
 
 data_sql = data_rdd.map(lambda l: l.split("\t")) \
-    .map(lambda d: Row(grupo=int(d[3])/franja))
+    .map(lambda d: Row(grupo=int(d[3])/franja, id_vehiculo=int(d[0])))
 
 data_df = sqlContext.createDataFrame(data_sql)
 
 data_df.registerTempTable('trafico')
 
-consulta = "SELECT grupo*%d AS limite_inferior, grupo*%d+%d AS limite_superior, COUNT(*) FROM trafico GROUP BY grupo ORDER BY grupo" % (franja, franja, franja)
+consulta = "SELECT grupo*%d AS limite_inferior, grupo*%d+%d AS limite_superior, COUNT(DISTINCT id_vehiculo) FROM trafico GROUP BY grupo ORDER BY grupo" % (franja, franja, franja)
 
 resultado = sqlContext.sql(consulta)
 
